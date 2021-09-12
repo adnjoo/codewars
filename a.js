6f515a487076ff2cdf68167c620cae4d718561e6
Andrew Njoo <adnjoo@gmail.com>
2021-09-11 21:31:42 -0700

    edit

--- a.js
+++ a.js
@@ -1,4 +1,7 @@
 // ooh one liner :)
-const isPalindrome = x =>  x.toLowerCase() == x.toLowerCase().split("").reverse().join("") ? true : false
-  
+const isPalindrome = (x) =>
+  x.toLowerCase() == x.toLowerCase().split("").reverse().join("")
+    ? true
+    : false;
+
 console.log(isPalindrome("Abba"));
