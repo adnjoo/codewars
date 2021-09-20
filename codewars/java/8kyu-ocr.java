adc51f2d427bb9bcfb9cfd52c76ba7190446864d
Andrew Njoo <adnjoo@gmail.com>
2021-09-20 02:17:11 -0700

    edit

---
+++ codewars/java/8kyu-ocr.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static String correct(String string) {
+    return string.replace('0','O').replace('1','I').replace('5','S');
+  }
+
+  // public static void main(String[] args) {
+  //   System.out.println(correct("L0ND0N"));
+  // }
+
+}
