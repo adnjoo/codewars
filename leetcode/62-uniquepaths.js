979d2f54c789a6a35d0991762db676f8795acfa5
Andrew Njoo <adnjoo@gmail.com>
2021-09-25 11:35:07 -0700

    rewrite

--- leetcode/62-uniquepaths.js
+++ leetcode/62-uniquepaths.js
@@ -3,23 +3,21 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n) {
-    return (factorial(m+n-2)/(factorial(m-1)*factorial(n-1)))
-};
 
+//factorial helper-fn
 var f = [];
-function factorial (n) {
-  if (n == 0 || n == 1)
-    return 1;
-  if (f[n] > 0)
-    return f[n];
-  return f[n] = factorial(n-1) * n;
+function factorial(n) {
+  if (n == 0 || n == 1) return 1;
+  if (f[n] > 0) return f[n];
+  return (f[n] = factorial(n - 1) * n);
 }
 
+var uniquePaths = (m, n) =>
+  factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1));
 
 // Input: m = 3, n = 7
-// (m + n - 2)! / ((m - 1)! * (n - 1)!). 
+// (m + n - 2)! / ((m - 1)! * (n - 1)!).
 // (3+7-2)!/((3-1)!*(7-1)!)
 // 8! / (2!*6!)
-// 40320/ 1440 
+// 40320/ 1440
 // 28
