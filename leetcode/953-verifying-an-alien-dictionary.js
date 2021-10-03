4a2cbe52ddd5d281cbd43d189d5beac0e221b666
Andrew Njoo <adnjoo@gmail.com>
2021-10-03 08:32:28 -0700

    edit

---
+++ leetcode/953-verifying-an-alien-dictionary.js
@@ -0,0 +1,25 @@
+var isAlienSorted = function (words, order) {
+  let dict = {};
+  order = order.split("").forEach((x, i) => (dict[x] = i));
+  // compare letter by letter
+  for (let i = 0; i < words.length - 1; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      // console.log(i, j, words[i][j], words[i + 1][j]);
+      if (
+        dict[words[i][j]] > dict[words[i + 1][j]] ||
+        dict[words[i + 1][j]] === undefined
+      )
+        return false;
+      if (dict[words[i][j]] < dict[words[i + 1][j]]) break; //if h comes before l break out of inner loop
+    }
+  }
+  return true;
+};
+
+// let words = ["hello", "leetcode"],
+//   order = "hlabcdefgijkmnopqrstuvwxyz";
+// let words = ["word", "world", "row"],
+//   order = "worldabcefghijkmnpqstuvxyz";
+let words = ["apple", "app"],
+  order = "abcdefghijklmnopqrstuvwxyz";
+console.log(isAlienSorted(words, order));
