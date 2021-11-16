94b73117887821e88517d90d4a22aae053d87aaa
Andrew Njoo <adnjoo@gmail.com>
2021-11-15 19:03:27 -0500

    edit

--- codewars/js/7kyu-youreasquare.js
+++ codewars/js/7kyu-youreasquare.js
@@ -1,1 +1,10 @@
+// better
+function isSquare(n) {
+    return Math.sqrt(n) % 1 === 0;
+}
+
+//another way
+const isSquare = n => Number.isInteger(Math.sqrt(n));
+
+//attempt
 const isSquare = (n) => n === 0 ? true : n < 0 ? false : Number.isInteger(Math.sqrt(n))
