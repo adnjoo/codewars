15dc1ba817722da0b8ea91eafb72f4a33623036e
Andrew Njoo <adnjoo@gmail.com>
2021-11-17 11:07:13 -0800

    refactor

--- codewars/js/6kyu-multiplesof3or5.js
+++ codewars/js/6kyu-multiplesof3or5.js
@@ -1,24 +1,23 @@
 // attempt 2
-function solution(number) {
-    return number <= 3
-      ? 0
-      : Array.from(Array(number).keys())
-          .filter((x) => x % 3 == 0 || x % 5 == 0)
-          .reduce((a, b) => a + b);
+const solution = (number) =>
+  number <= 3
+    ? 0
+    : Array.from(Array(number).keys())
+        .filter((x) => x % 3 == 0 || x % 5 == 0)
+        .reduce((a, b) => a + b);
+
+// attempt 1
+function solution2(number) {
+  let arr = [];
+  if (number <= 3) {
+    return 0;
   }
-  
-  // attempt 1
-  function solution2(number) {
-    let arr = [];
-    if (number <= 3) {
-      return 0;
+  for (i = 3; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      arr.push(i);
     }
-    for (i = 3; i < number; i++) {
-      if (i % 3 == 0 || i % 5 == 0) {
-        arr.push(i);
-      }
-    }
-    return arr.reduce((a, b) => a + b);
   }
-  
-  console.log(solution(20));
+  return arr.reduce((a, b) => a + b);
+}
+
+console.log(solution(20));
