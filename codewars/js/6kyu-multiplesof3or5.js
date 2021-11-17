5410ef923f7824949b5d6c7e0ab533970aa9d3b7
Andrew Njoo <adnjoo@gmail.com>
2021-11-17 11:05:59 -0800

    add 6kyu

---
+++ codewars/js/6kyu-multiplesof3or5.js
@@ -0,0 +1,24 @@
+// attempt 2
+function solution(number) {
+    return number <= 3
+      ? 0
+      : Array.from(Array(number).keys())
+          .filter((x) => x % 3 == 0 || x % 5 == 0)
+          .reduce((a, b) => a + b);
+  }
+  
+  // attempt 1
+  function solution2(number) {
+    let arr = [];
+    if (number <= 3) {
+      return 0;
+    }
+    for (i = 3; i < number; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        arr.push(i);
+      }
+    }
+    return arr.reduce((a, b) => a + b);
+  }
+  
+  console.log(solution(20));
