cfb0f2e25ae4c2ebad9e21bfaabef6bdf8687917
Andrew Njoo <adnjoo@gmail.com>
2021-11-23 14:37:27 -0800

    edit

--- leetcode/2078-twofurthesthouses.js
+++ leetcode/2078-twofurthesthouses.js
@@ -2,14 +2,14 @@
  * @param {number[]} colors
  * @return {number}
  */
- var maxDistance = function(colors) {
-    let max = 0;
-    for(let i = 0; i < colors.length; i ++){
-        for(let j = i + 1; j < colors.length; j++){
-            if(colors[i]!=colors[j]){
-                max = Math.max(max, Math.abs(j-i))
-            }
-        }
+var maxDistance = function (colors) {
+  let max = 0;
+  for (let i = 0; i < colors.length; i++) {
+    for (let j = i + 1; j < colors.length; j++) {
+      if (colors[i] != colors[j]) {
+        max = Math.max(max, Math.abs(j - i));
+      }
     }
-    return max
-  };
+  }
+  return max;
+};
