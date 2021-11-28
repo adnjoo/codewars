8f89f0be8f29118701eddff20d17168b8580e779
Andrew Njoo <adnjoo@gmail.com>
2021-11-28 10:23:25 -0800

    edit

---
+++ leetcode/2037-minimummovestoseateveryone.js
@@ -0,0 +1,23 @@
+let sortInt = (a, b) => a - b;
+
+var minMovesToSeat = function (seats, students) {
+  let counter = 0;
+  seats.sort(sortInt);
+  students.sort(sortInt);
+  for (let i in seats) {
+    counter += Math.abs(seats[i] - students[i]);
+  }
+  return counter;
+};
+
+//seats, students
+let x = [3, 1, 5],
+  y = [2, 7, 4];
+let x1 = [4, 1, 5, 9],
+  y1 = [1, 3, 2, 6];
+let x2 = [2, 2, 6, 6],
+  y2 = [1, 3, 2, 6];
+
+console.log(minMovesToSeat(x, y));
+minMovesToSeat(x1, y1);
+minMovesToSeat(x2, y2);
