179e2899cf6111a423c4ecbdc189b3cba6152248
Andrew Njoo <adnjoo@gmail.com>
2021-11-28 12:33:01 -0800

    edit

---
+++ leetcode/1974-minimumtimetotypeword.js
@@ -0,0 +1,15 @@
+var minTimeToType = function (word) {
+    word = word.split("");
+    let alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+    let counter = 0;
+    let pointer = "a";
+    //find distance from a to word[i] both cw and ccw, take the minimum
+    for (let i in word) {
+      //cw | ccw | indexof letter, pointer
+      let cw = Math.abs(alpha.indexOf(word[i]) - alpha.indexOf(pointer));
+      counter += Math.min(cw, Math.abs(26 - cw)) + 1; //+1 to type char pointer is on
+      pointer = word[i];
+    }
+    return counter;
+  };
+  
