8660c2eb41994e67a2a87b9ce6d7dfff40829ddd
Andrew Njoo <adnjoo@gmail.com>
2021-12-04 07:37:48 -0800

    edit

---
+++ leetcode/804-uniquemorsecodewords.js
@@ -0,0 +1,46 @@
+var uniqueMorseRepresentations = function (words) {
+    let morse = [
+      ".-",
+      "-...",
+      "-.-.",
+      "-..",
+      ".",
+      "..-.",
+      "--.",
+      "....",
+      "..",
+      ".---",
+      "-.-",
+      ".-..",
+      "--",
+      "-.",
+      "---",
+      ".--.",
+      "--.-",
+      ".-.",
+      "...",
+      "-",
+      "..-",
+      "...-",
+      ".--",
+      "-..-",
+      "-.--",
+      "--..",
+    ];
+    let alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+    let arr = [];
+    words.forEach((el) => {
+      let str = "";
+      el.split("").forEach((char) => {
+        str = str.concat(morse[alpha.indexOf(char)]);
+      });
+      arr.push(str);
+    });
+    //remove duplicates
+    return [...new Set(arr)].length;
+  };
+  let w1 = ["gin", "zen", "gig", "msg"],
+    w2 = ["a"];
+  
+  console.log(uniqueMorseRepresentations(w1), uniqueMorseRepresentations(w2));
+  
