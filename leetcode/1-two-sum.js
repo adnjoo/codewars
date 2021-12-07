b0316b35d87e252d1e305ddb59f05013c3952339
Andrew Njoo <adnjoo@gmail.com>
2021-12-06 18:03:11 -0800

    add sol for twosum

--- leetcode/1-two-sum.js
+++ leetcode/1-two-sum.js
@@ -1,3 +1,20 @@
+//hash map
+//time complexity: O(n)
+//space complexity: O(n)
+const twoSum = (n, t) => {
+  let hash = {};
+  for (let i in n) {
+    if (hash[n[i]] ) {
+      return [i, hash[n[i]]];
+    } else {
+      hash[t - n[i]] = i;
+    }
+  }
+};
+
+//nested for
+//time complexity: O(n2)
+//space complexity: O(1)
 var twoSum = function (n, t) {
     for (i = 0; i < n.length; i++) {
       for (j = i + 1; j < n.length; j++) {
