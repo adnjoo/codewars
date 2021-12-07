ad5d38ee957e16751dce642086ef53d67244c9c0
Andrew Njoo <adnjoo@gmail.com>
2021-12-07 14:17:20 -0800

    add 344

---
+++ leetcode/0344-reverse.string.js
@@ -0,0 +1,12 @@
+// another way
+var reverseString = function(s) {
+    for(let i =0,j=s.length-1;i<j;i++, j--){
+        const x = s[i]
+        const y = s[j]  
+        s[i]=y
+        s[j]=x
+    }
+};
+
+//fast
+const reverseString = (s)=>  s.reverse()
