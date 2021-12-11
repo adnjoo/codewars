d69e11470bd2398550264333eb9daebe4e146d6f
Andrew Njoo <adnjoo@gmail.com>
2021-12-11 10:54:56 -0800

    format code

--- leetcode/0001-two-sum.py
+++ leetcode/0001-two-sum.py
@@ -1,8 +1,9 @@
-#nested for loop
-def twoSum(nums,target):
+# nested for loop
+def twoSum(nums, target):
     for i in range(len(nums)-1):
-        for j in range(i+1,len(nums)):
-            if(nums[i]+nums[j]==target):
-                return [i,j]
+        for j in range(i+1, len(nums)):
+            if(nums[i]+nums[j] == target):
+                return [i, j]
 
-print(twoSum([2,7,11,15],9))
+
+print(twoSum([2, 7, 11, 15], 9))
