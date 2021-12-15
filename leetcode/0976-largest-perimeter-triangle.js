594f9bc016ce9e705c94438c69b3d3edf516fda6
Andrew Njoo <adnjoo@gmail.com>
2021-12-15 12:05:52 -0800

    refactor 976

--- leetcode/0976-largest-perimeter-triangle.js
+++ leetcode/0976-largest-perimeter-triangle.js
@@ -1,32 +1,28 @@
 var largestPerimeter = function (nums) {
-    nums = nums.sort((a, b) => a - b);
-    //start with largest side
-    //test all other sides
-    let triangle = [];
-    for (let i = nums.length - 1; i >= 0; i--) {
-      let a = nums[i];
-      let b = nums[i - 1];
-      let c = nums[i - 2];
-      if (b + c > a) {
-        triangle.push(a, b, c);
-        return triangle.reduce((a, b) => a + b);
-      }
+  nums = nums.sort((a, b) => a - b);
+  let triangle = [];
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let a = nums[i];
+    let b = nums[i - 1];
+    let c = nums[i - 2];
+    if (b + c > a) {
+      return a + b + c;
     }
-    return 0;
-  };
-  
-  let n1 = [2, 1, 2],
-    n2 = [1, 2, 1],
-    n3 = [3, 2, 3, 4],
-    n4 = [3, 6, 2, 3];
-  
-  console.log(
-    largestPerimeter(n1),
-    largestPerimeter(n2),
-    largestPerimeter(n3),
-    largestPerimeter(n4)
-  );
-  
-  // find largest perimeter of a triangle with sides a, b, c
-  // where a + b > c
-  
+  }
+  return 0;
+};
+
+let n1 = [2, 1, 2],
+  n2 = [1, 2, 1],
+  n3 = [3, 2, 3, 4],
+  n4 = [3, 6, 2, 3];
+
+console.log(
+  largestPerimeter(n1),
+  largestPerimeter(n2),
+  largestPerimeter(n3),
+  largestPerimeter(n4)
+);
+
+// find largest perimeter of a triangle with sides a, b, c
+// where a + b > c
