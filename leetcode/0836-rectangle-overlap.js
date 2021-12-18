683e88aa94902dbd24d1abcc8617c6c6db8b0373
Andrew Njoo <adnjoo@gmail.com>
2021-12-17 18:34:18 -0800

    add notes for 836

--- leetcode/0836-rectangle-overlap.js
+++ leetcode/0836-rectangle-overlap.js
@@ -1,3 +1,15 @@
+  /*
+  0, 1, 2, 3
+  x1, y1, x2, y2
+  all conditions must be valid for overlap
+  1: x1(rec1) < x2(rec2)
+  2: x2(rec1) > x1(rec2)
+  3: y1(rec1) < y2(rec2)
+  4: y2(rec1) > y1(rec2)
+
+  as shown in rec1 = [0,0,2,2] and rec2 = [1,1,3,3]
+  */
+
 var isRectangleOverlap = function (rec1, rec2) {
   return (
     rec1[0] < rec2[2] &&
