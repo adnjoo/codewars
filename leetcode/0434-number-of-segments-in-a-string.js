d6689e5fae0fb4a87fa50c1278bb9efbdfc4725b
Andrew Njoo <adnjoo@gmail.com>
2021-12-24 12:39:32 -0800

    edit 434

--- leetcode/0434-number-of-segments-in-a-string.js
+++ leetcode/0434-number-of-segments-in-a-string.js
@@ -1,6 +1,6 @@
 // A segment is defined to be a contiguous sequence of non-space characters.
-const countSegments = (s) =>
-  s == "" ? 0 : s.split(" ").filter((x) => !x.length == 0).length;
+const countSegments = (s) =>  s.split(' ').filter(x=>!x.length==0).length
+
 
 let s1 = "Hello, my name is John",
   s2 = "Hello",
