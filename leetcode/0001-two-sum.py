2089b9dc0884452909c203894846c9102d0a80ec
Andrew Njoo <adnjoo@gmail.com>
2022-04-10 07:58:19 -0700

    add solution to 0001

--- leetcode/0001-two-sum.py
+++ leetcode/0001-two-sum.py
@@ -1,9 +1,21 @@
-# nested for loop
+# TC: O(1) - hashmap lookup
+# utilize hashmap
 def twoSum(nums, target):
-    for i in range(len(nums)-1):
-        for j in range(i+1, len(nums)):
-            if(nums[i]+nums[j] == target):
-                return [i, j]
+    dict = {}
+    for i, n in enumerate(nums):
+        m = target - n
+        if m in dict:
+            return [dict[m], i]
+        else:
+            dict[n] = i
+
+# TC: O N^2
+# nested for loop
+# def twoSum(nums, target):
+#     for i in range(len(nums)-1):
+#         for j in range(i+1, len(nums)):
+#             if(nums[i]+nums[j] == target):
+#                 return [i, j]
 
 
 print(twoSum([2, 7, 11, 15], 9))
